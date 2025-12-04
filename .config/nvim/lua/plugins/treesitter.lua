862030b455c4b1bb45f9f41d44a1e347cf27e584
Milutin Popovic <milutin@popovic.xyz>
2025-12-04 21:35:52 +0000

    out

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ":TSUpdate",
 	config = function ()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = "all",
+--			ensure_installed = "none",
 			auto_install = true,
 			indent = {
 				enable = true
