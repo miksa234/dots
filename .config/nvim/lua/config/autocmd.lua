daad8e227346bffa9b5f6bf896231c139db9c58a
Milutin Popovic <milutin@popovic.xyz>
2026-03-10 21:53:33 +0000

    update

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -21,30 +21,30 @@ autocmd({'BufWritePost'}, {
 	command = '!xrdb %',
 })
 
-autocmd({'BufWritePost'}, {
-	pattern = vim.fn.expand('~') .. '/.local/src/dmenu/config.def.h',
-	command = '!cd ~/.local/src/dmenu/; sudo make clean install && { killall -q dmenu}',
-})
-
-autocmd({'BufWritePost'}, {
-	pattern = vim.fn.expand('~') .. '/.local/src/dwm/config.def.h',
-	command = '!cd ~/.local/src/dwm/; sudo make clean install && { killall -q dwm;setsid dwm & }',
-})
-
-autocmd({'BufWritePost'}, {
-	pattern = vim.fn.expand('~') .. '/.local/src/dwmblocks/config.h',
-	command = '!cd ~/.local/src/dwmblocks/; sudo make clean install && { killall -q dwmblocks;setsid dwmblocks & }',
-})
-
-autocmd({'BufWritePost'}, {
-	pattern = vim.fn.expand('~') .. '/.local/src/st/config.h',
-	command = '!cd ~/.local/src/st/; sudo make clean install && { killall -q st;setsid st & }',
-})
-
-autocmd({'BufWritePost'}, {
-	pattern = vim.fn.expand('~') .. '/.local/src/slock/config.h',
-	command = '!cd ~/.local/src/slock/; sudo make clean install && { killall -q slock;setsid & }',
-})
+--autocmd({'BufWritePost'}, {
+--	pattern = vim.fn.expand('~') .. '/.local/src/dmenu/config.def.h',
+--	command = '!cd ~/.local/src/dmenu/; sudo make clean install && { killall -q dmenu}',
+--})
+--
+--autocmd({'BufWritePost'}, {
+--	pattern = vim.fn.expand('~') .. '/.local/src/dwm/config.def.h',
+--	command = '!cd ~/.local/src/dwm/; sudo make clean install && { killall -q dwm;setsid dwm & }',
+--})
+--
+--autocmd({'BufWritePost'}, {
+--	pattern = vim.fn.expand('~') .. '/.local/src/dwmblocks/config.h',
+--	command = '!cd ~/.local/src/dwmblocks/; sudo make clean install && { killall -q dwmblocks;setsid dwmblocks & }',
+--})
+--
+--autocmd({'BufWritePost'}, {
+--	pattern = vim.fn.expand('~') .. '/.local/src/st/config.h',
+--	command = '!cd ~/.local/src/st/; sudo make clean install && { killall -q st;setsid st & }',
+--})
+--
+--autocmd({'BufWritePost'}, {
+--	pattern = vim.fn.expand('~') .. '/.local/src/slock/config.h',
+--	command = '!cd ~/.local/src/slock/; sudo make clean install && { killall -q slock;setsid & }',
+--})
 
 -- Convert two spaces to tabs in router folder
 autocmd({'BufWritePre'}, {
