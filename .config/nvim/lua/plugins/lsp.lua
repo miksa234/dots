371584034773e95c31d198c974917a1fe8a3064c
Milutin Popovic <milutin@popovic.xyz>
2026-03-11 12:00:00 +0000

    sytax

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -143,6 +143,7 @@ return {
                 workspace = {
                   checkThirdParty = false,
                   library = vim.api.nvim_get_runtime_file("", true)
+                }
               })
             end,
             settings = {
