77e6504385113bb99ed92200909f6fb3e42bf210
Milutin Popovic <milutin@popovic.xyz>
2026-04-04 00:34:43 +0100

    nvim fixes and trm dmenu-niri

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -68,6 +68,7 @@ return {
     })
 
     require("fidget").setup({})
+
     require("mason").setup()
     require("mason-lspconfig").setup({
       handlers = {
@@ -133,22 +134,17 @@ return {
             settings = {
               Lua = {
                 runtime = {
-                  -- Tell the language server which version of Lua you're using
-                  -- (most likely LuaJIT in the case of Neovim)
                   version = 'LuaJIT',
                 },
                 diagnostics = {
-                  -- Get the language server to recognize the `vim` global
                   globals = {
                     'vim',
                     'require'
                   },
                 },
                 workspace = {
-                  -- Make the server aware of Neovim runtime files
                   library = vim.api.nvim_get_runtime_file("", true),
                 },
-                -- Do not send telemetry data containing a randomized but unique identifier
                 telemetry = {
                   enable = false,
                 },
