664de86d4bdb0b5088e99e2dfe65ecb32f27297c
Milutin Popovic <milutin@popovic.xyz>
2026-04-10 22:41:12 +0100

    remove nvim config

--- .config/nvim/after/ftplugin/tex.lua
+++
@@ -1,3 +0,0 @@
-vim.g.tex_flavor = "latex"
-vim.bo.textwidth = 77
-vim.wo.wrap = true
--- .config/nvim/lua/plugins/autorepair.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'windwp/nvim-autopairs',
-  event = "InsertEnter",
-  config = true,
-  disable_filetype = { "TelescopePrompt" , "vim" },
-}
